4f43b35fb486b3bf44baeba54923386656c3aad0
Ajeesh Vijayan <ajeesh_vijayan@intuit.com>
2016-06-12 00:45:09 -0700

    renamed the core package to awscore

--- setup.py
+++ setup.py
@@ -5,7 +5,32 @@ setup(
     version='1.0',
     packages=['core'],
     url='',
-    license='The MIT License (MIT)',
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='aws command line tool',
+    install_requires=['boto3', 'prettytable'],
+
+    entry_points={
+        'console_scripts': [
+            'awsview=awsview:main',
+        ],
+    },
+
     author='Ajeesh T Vijayan',
     author_email='ajeeshvt@gmail.com',
     description='A simple python application to view the AWS account resources in a tabular format'
