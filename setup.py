2a59d12d83e9899fbd8c87ae97ab00857731e292
Ajeesh Vijayan <ajeesh_vijayan@intuit.com>
2016-06-14 02:00:47 -0700

    removed entrypoints from setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,13 @@ from distutils.core import setup
 
 setup(
     name='AwsViewCmdConsole',
-    version='1.0.1',
+    version='1.0.2',
     packages=['awscore'],
     scripts=['scripts/awsview'],
     url='https://github.com/ajeeshvt/AwsViewCmdConsole',
-    download_url='https://github.com/ajeeshvt/AwsViewCmdConsole/tarball/1.0.1',
+    download_url='https://github.com/ajeeshvt/AwsViewCmdConsole/tarball/1.0.2',
     license='MIT',
     install_requires=['boto3', 'prettytable'],
-
-    entry_points={
-        'console_scripts': [
-            'awsview=awsview:main',
-        ],
-    },
-
     author='Ajeesh T Vijayan',
     author_email='ajeeshvt@gmail.com',
     description='A simple python application to view the AWS account resources in a tabular format'
