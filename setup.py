22dd8386a0f43fd2f9e8eaf57b0b8eccc0cd3b0a
Ajeesh Vijayan <ajeesh_vijayan@intuit.com>
2016-06-14 01:44:02 -0700

    version change in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='AwsViewCmdConsole',
-    version='1.0',
+    version='1.0.0',
     packages=['awscore'],
     scripts=['scripts/awsview'],
     url='https://github.com/ajeeshvt/AwsViewCmdConsole',
