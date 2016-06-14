f08c2b24bed019de390c31b3c789809426a47aa8
Ajeesh Vijayan <ajeesh_vijayan@intuit.com>
2016-06-14 01:48:39 -0700

    version change in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='AwsViewCmdConsole',
-    version='1.0.0',
+    version='1.0.1',
     packages=['awscore'],
     scripts=['scripts/awsview'],
     url='https://github.com/ajeeshvt/AwsViewCmdConsole',
+    download_url='https://github.com/ajeeshvt/AwsViewCmdConsole/tarball/1.0.1',
     license='MIT',
     install_requires=['boto3', 'prettytable'],
 
