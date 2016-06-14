2f110fd8c47a0567d93ddba743018e07c63fd123
Ajeesh Vijayan <ajeesh_vijayan@intuit.com>
2016-06-14 01:04:18 -0700

    added cfstack info and route53 info

--- setup.py
+++ setup.py
@@ -3,9 +3,18 @@ from distutils.core import setup
 setup(
     name='AwsViewCmdConsole',
     version='1.0',
-    packages=['core'],
+    packages=['awscore'],
+    scripts=['scripts/awsview'],
     url='',
-    license='The MIT License (MIT)',
+    license='MIT',
+    install_requires=['boto3', 'prettytable'],
+
+    entry_points={
+        'console_scripts': [
+            'awsview=awsview:main',
+        ],
+    },
+
     author='Ajeesh T Vijayan',
     author_email='ajeeshvt@gmail.com',
     description='A simple python application to view the AWS account resources in a tabular format'
